3d7ba6ddbcff9dbce6d8433ac0f326ae7ad33d77
Aaron Dixon <aarondixon@Grey-Goose.local>
2025-09-27 10:43:08 -0500

    hello-world drag demo

--- hello-world/src/main.ts
+++ hello-world/src/main.ts
@@ -4,7 +4,16 @@ import {
     XellyInstallFunction,
     XellyMetadata
 } from '@xelly/xelly.js';
-import {Color, Engine, Font, FontUnit, Label, vec} from 'excalibur';
+import {
+    Actor,
+    Color,
+    Engine,
+    Font,
+    FontUnit,
+    Label,
+    vec,
+    Vector
+} from 'excalibur';
 
 const font24 = new Font({
     color: Color.Black,
@@ -15,18 +24,50 @@ const font24 = new Font({
 
 /** Metadata. */
 export const metadata: XellyMetadata = {
-    type: XellyGameType.Passive
+    //type: XellyGameType.Passive
+    type: XellyGameType.Realtime
+};
+
+const makeDraggable = (engine: Engine, actor: Actor) => {
+    let dragOffset: Vector | undefined;
+    actor.on('pointerdragstart', (event) => {
+        dragOffset = event.screenPos.sub(actor.pos);
+    });
+    actor.on('pointerdragend', (event) => {
+        dragOffset = undefined;
+    });
+    engine.input.pointers.primary.on('move', event => {
+        // using top-level 'move' event gives better experience than
+        //   actor.on('pointerdragmove', ...) in the case the
+        //    pointer ends up not directly over actor during moving:
+        if (dragOffset) {
+            actor.pos = event.screenPos.sub(dragOffset)
+        }
+    });
+    /*actor.on('pointerdragmove', (event) => {
+        if (dragOffset) {
+            actor.pos = event.screenPos.sub(dragOffset)
+        }
+    });
+    engine.input.pointers.primary.on('down', event => {
+        // no-op
+    });
+    engine.input.pointers.primary.on('up', event => {
+        dragOffset = undefined;
+    });*/
 };
 
 /** Install. */
 export const install: XellyInstallFunction = (context: XellyContext, engine: Engine) => {
     const message = "Hello, world!";
     const messageDimensions = font24.measureText(message);
-    engine.add(new Label({
+    const label = new Label({
         text: message,
         font: font24,
         pos: vec(
             (engine.drawWidth - messageDimensions.width) / 2,
             (engine.drawHeight - messageDimensions.height) / 2),
-    }));
+    });
+    engine.add(label);
+    makeDraggable(engine, label);
 };
